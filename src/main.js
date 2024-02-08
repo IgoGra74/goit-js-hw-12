92e1863c172d51e9539b8ed859e8b5485ee78b0e
IgorGra1 <gis1@ukr.net>
2024-02-08 22:42:54 +0200

    update

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ formSearch.addEventListener('submit', onFormSubmit);
 
 const lightbox = new SimpleLightbox('.gallery a', {});
 
-function onFormSubmit(event) {
+async function onFormSubmit(event) {
   event.preventDefault();
 
   const query = event.target.elements.query.value.trim();
@@ -32,70 +32,65 @@ function onFormSubmit(event) {
   galleryContainer.innerHTML = '';
   loader.style.display = 'block';
 
-  getImage(query)
-    .then(function ({ hits, total }) {
-      if (Array.isArray(hits) && hits.length > 0 && total > 0) {
-        renderImage(hits);
+  try {
+    const data = await getImage(query);
 
-        lightbox.refresh();
-      } else {
-        iziToast.warning({
-          titleColor: '#FFFFFF',
-          message:
-            'Sorry, there are no images matching your search query. Please try again.',
-          messageSize: '16px',
-          position: 'topRight',
-          messageColor: '#FFFFFF',
-          backgroundColor: '#EF4040',
-          iconUrl: null,
-        });
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-      iziToast.error({
-        title: 'Error',
+    if (Array.isArray(data.hits) && data.hits.length > 0 && data.total > 0) {
+      renderImage(data.hits);
+
+      lightbox.refresh();
+    } else {
+      iziToast.warning({
         titleColor: '#FFFFFF',
-        message: 'An error occurred while fetching data. Please try again.',
+        message:
+          'Sorry, there are no images matching your search query. Please try again.',
+        messageSize: '16px',
         position: 'topRight',
         messageColor: '#FFFFFF',
-        backgroundColor: '#FF6347',
+        backgroundColor: '#EF4040',
         iconUrl: null,
       });
-    })
-    .finally(function () {
-      event.target.reset();
-      loader.style.display = 'none';
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    iziToast.error({
+      title: 'Error',
+      titleColor: '#FFFFFF',
+      message: 'An error occurred while fetching data. Please try again.',
+      position: 'topRight',
+      messageColor: '#FFFFFF',
+      backgroundColor: '#FF6347',
+      iconUrl: null,
     });
+  } finally {
+    event.target.reset();
+    loader.style.display = 'none';
+  }
 }
 
-function getImage(hit) {
+async function getImage(hit) {
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
   const PARAMS = `?key=42099926-52a1046a87902a6e56a7e135a&q=${hit}&image_type=photo&orientation=horizontal&safesearch=true `;
 
   const url = BASE_URL + END_POINT + PARAMS;
 
-  return fetch(url)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
-      }
-      return res.json();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        titleColor: '#FFFFFF',
-        message: 'An error occurred while fetching data. Please try again.',
-        position: 'topRight',
-        messageColor: '#FFFFFF',
-        backgroundColor: '#FF6347',
-        iconUrl: null,
-      });
-      console.error('Error fetching data:', error);
-      throw error;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      titleColor: '#FFFFFF',
+      message: 'An error occurred while fetching data. Please try again.',
+      position: 'topRight',
+      messageColor: '#FFFFFF',
+      backgroundColor: '#FF6347',
+      iconUrl: null,
     });
+    console.error('Error fetching data:', error);
+    throw error;
+  }
 }
 
 function imageTemplate({
@@ -148,7 +143,7 @@ function imagesTemplate(hits) {
   return hits.map(imageTemplate).join('');
 }
 
-function renderImage(Hits) {
-  const markup = imagesTemplate(Hits);
+function renderImage(hits) {
+  const markup = imagesTemplate(hits);
   galleryContainer.innerHTML = markup;
 }
